d6ffdb49c91da4920ce49dbce4f441ea59e4779b
Aderbal Farias <aderbalfarias@hotmail.com>
2019-07-14 19:29:46 +0100

    Adding route module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    { path: '**', component: HomeComponent }
+];
+
+@NgModule({
+    exports: [RouterModule],
+    imports: [
+        RouterModule.forRoot(routes)
+    ]
+})
+
+export class AppRoutingModule { }
