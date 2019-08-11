d16b83b2a28e760a183869ce1d7bb3b7719064b2
Aderbal Farias <aderbalfarias@hotmail.com>
2019-08-11 17:07:39 +0100

    Fixing issue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
-import { AboutComponent } from './about/about.component';
+import { AboutComponent } from './components/about/about.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
